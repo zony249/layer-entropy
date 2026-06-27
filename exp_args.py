f0762222227c85a8ff69c06b8b18220c0caca581
Zony Yu <zony249@gmail.com>
2026-06-27 12:16:57 -0600

    new arguments

--- exp_args.py
+++ exp_args.py
@@ -1,7 +1,7 @@
 # Experiment Arguments; i.e., arguments pertaining to various experimental setups, rather than training or eval hyperparameters.
-import os 
-import sys 
-from argparse import Namespace, ArgumentParser 
+import os
+import sys
+from argparse import Namespace, ArgumentParser
 from typing import Dict, List, Tuple
 
 
@@ -9,7 +9,7 @@ from typing import Dict, List, Tuple
 def parse_exp_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("--add_gist", action="store_true", help="Whether or not to inject gist tokens")
-    parser.add_argument("--compression_rate", type=int, default=5, help="context:gist ratio")
+    parser.add_argument("--compression_rate", type=float, default=None, help="context:gist ratio")
     parser.add_argument("--attention_mask_mode", type=str, default="compression", choices=["compression", "full", "contextless"], help="compression mask is used for all compression scenarios")
     parser.add_argument("--compression_mode", default="none", choices=["none", "fourier", "average"], help="deterministic function used for compression. For learned compression, select 'none'")
     parser.add_argument("--gist_scheme", type=str, default="end", choices=["end", "dispersed"])
@@ -21,20 +21,22 @@ def parse_exp_args() -> Namespace:
     parser.add_argument("--entropy_model_temp", type=float, default=1)
     ### END DEPRECATED ###
 
-    parser.add_argument("--act_guided_chunking", type=str, default=None, choices=[None, "normdiff"], 
+    parser.add_argument("--act_guided_chunking", type=str, default=None, choices=[None, "normdiff", "min_chunk_diff", "reg_cosine"],
                         help="""activation-guided chunking.
                             normdiff: chunking based on norm of difference vectors between tokens
                         """)
     parser.add_argument("--chunking_model", type=str, default=None, help="Chunking model used to help guide gist dispersion")
+    parser.add_argument("--use_layers", type=int, nargs="+", default=None)
 
 
-    parser.add_argument("--nltk_chunker", type=str, default=None, choices=[None, ""])
+    parser.add_argument("--nltk_chunker", nargs="+", default=None, choices=[None, "np", "vp"])
+    parser.add_argument("--alpha_unif", type=float, default=0)
 
 
     args, unknown = parser.parse_known_args()
     return args
 
-def join_args(*list_args: List[Namespace]) -> Namespace: 
+def join_args(*list_args) -> Namespace:
     joint_dict = {k: v for a in list_args for k, v in vars(a).items()}
     return Namespace(**joint_dict)
 
