ad21ba4fd86f253c827e85465d7afb5d885912c4
Zony Yu <zony249@gmail.com>
2026-08-24 10:29:40 -0600

    testing binary mask

--- test_mask.py
+++ test_mask.py
@@ -17,65 +17,40 @@ if __name__ == "__main__":
 
     torch.autograd.set_detect_anomaly(True)
 
-    token_annotation = torch.tensor([0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2])
-    # token_annotation = torch.tensor([0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
+    # token_annotation = torch.tensor([0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2])
+    token_annotation = torch.tensor([0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
 
-    # token_annotation = []
-    # for i in range(50): 
-    #     token_annotation.append(torch.tensor([0, 0, 0, 0, 0])) 
-    #     token_annotation.append(torch.tensor([1]))
-    # token_annotation.append(torch.tensor([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]))
-    # token_annotation = torch.cat(token_annotation, dim=0) 
+    token_annotation = []
+    for i in range(50): 
+        token_annotation.append(torch.tensor([0, 0, 0, 0, 0])) 
+        token_annotation.append(torch.tensor([1]))
+    token_annotation.append(torch.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+    token_annotation = torch.cat(token_annotation, dim=0) 
 
     seq_len = token_annotation.shape[0]
     alpha = 0.1
-    z = F.one_hot(token_annotation, num_classes=3).float()
-    P = z * (1-alpha) + alpha / 3
+    P = F.one_hot(token_annotation, num_classes=2).float()
     # P = token_annotation.float()
     P.requires_grad = True
     P_same = torch.zeros((seq_len, seq_len))
 
-    # tbar = tqdm(range(4), desc="Running") 
-    # for _ in tbar:
-    #     for j in range(seq_len):
-    #         inside = 1
-    #         for i in range(j+1): 
-    #             val = torch.prod(P[i:j, 0]) 
-    #             if i == j: 
-    #                 val *= (P[j, 0] + P[j, 1])
-    #             P_same[j, i] = val
-
-    #     M = torch.zeros((seq_len, seq_len)) 
-    #     for j in range(seq_len): 
-    #         for i in range(j+1): 
-    #             M[j, i] = P_same[j, i] + (1 - P_same[j, i]) * (P[i, 1] * P[j, 2] + P[i, 2] * P[j, 2])
     
     tbar = tqdm(range(1000), desc="Running") 
     for _ in tbar:
-        log_P = torch.log(P * (1-1e-8) + 1e-8) 
-        clog_P_j = torch.cumsum(log_P[:, 0], dim=0) - log_P[:, 0] + (torch.log(P[:, 0] + P[:, 1]))
-        clog_P_i = torch.cumsum(log_P[:, 0], dim=0) - log_P[:, 0]
-        clog_P_ji = clog_P_j[:, None] - clog_P_i[None, :] 
-        P_same = torch.tril(torch.exp(clog_P_ji))
-        M = torch.zeros((seq_len, seq_len))
-        Pji_bound_downstream = torch.tril(P[:, 2][:, None] * P[:, 1][None, :])
-        Pji_downstream = torch.tril(P[:, 2][:, None] * P[:, 2][None, :])
-        Pji_bounds = torch.tril(P[:, 1][:, None] * P[:, 1][None, :])
-        M = P_same + (1-P_same) * (Pji_bound_downstream + Pji_downstream + Pji_bounds)
-        break
 
-    # binary class version
-    for _ in tbar: 
-        log_P = torch.log((1-P).clamp(min=1e-8))
-        clog_P = torch.cumsum(log_P, dim=0)
-        clog_P_ij = clog_P[: , None] - clog_P[None, :]  + log_P[None, :] - log_P[:, None] 
-        print(clog_P_ij)
-        causal_mask = torch.tril(torch.ones(clog_P_ij.shape, dtype=bool))
+        P_dtype = P.dtype
+        P = P.float()
+        log_P = torch.log(P[:, 0].clamp(min=1e-8)) 
+        clog_P = torch.cumsum(log_P, dim=0) 
+        clog_P_ij = clog_P[:, None] - clog_P[None, :] + log_P[None, :] - log_P[:, None] 
+        causal_mask = torch.tril(torch.ones(clog_P_ij.shape, dtype=bool, device=P.device))
         log_P_same = torch.where(causal_mask, clog_P_ij, torch.finfo(P.dtype).min * 1e-2) 
-        P_same = torch.exp(log_P_same) 
-        P_end = torch.tril(P[:, None] * P[None, :])
+        P_same = torch.exp(log_P_same)
+        P_end = torch.tril(P[:, 1][:, None] * P[:, 1][None, :])
         M = P_same  + (1-P_same) * P_end
-        break
+        M = M.to(P_dtype)
+
+
 
 
     M.sum().backward()
@@ -83,5 +58,5 @@ if __name__ == "__main__":
 
     print(M.min(), M.max())
     
-    plt.imshow(M.numpy()) 
+    plt.imshow(M.detach().cpu().numpy()) 
     plt.savefig("runs/softmask.png", dpi=300)
